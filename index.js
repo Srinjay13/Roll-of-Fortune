87040a4422cf347c2f494b38c7c3a0e0e2f7ad80
Srinjay saha <srinjay.it220002@bppimt.ac.in>
2023-06-19 22:46:15 +0530

    Javascript File

---
+++ index.js
@@ -0,0 +1,20 @@
+
+function ref(){
+    let p1= Math.ceil(Math.random()*5);
+    let p2= Math.ceil(Math.random()*5);
+    let s1_p1="images/dice"+p1+".png";
+    let s2_p2="images/dice"+p2+".png";
+    document.querySelector(".img1").setAttribute("src",s1_p1);
+    document.querySelector(".img2").setAttribute("src",s2_p2);
+    if(p1>p2)
+    {
+        document.querySelector("h2").innerHTML="🚩Player 1 wins !";
+    }
+    else if(p1<p2){
+    document.querySelector("h2").innerHTML="🚩Player 2 wins !";
+    }
+    else{
+        document.querySelector("h2").innerHTML="🚩No winner.Chicken dinner shared !"
+    }
+
+}
